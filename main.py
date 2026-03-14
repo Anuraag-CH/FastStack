091fac474a4811f7f9b980e0bac8d95da2f4a7e3
Anuraag-CH <anuraag.ch@gmail.com>
2026-03-15 01:30:41 +0530

    Add jinja2 templates

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 posts = [
     {
     "id": 1,
     "title": "FastAPI",
     "content": "FastAPI is a modern, fast (high-performance), web framework for building APIs with Python 3.7+.",
-    "created_at": "2026-03-12",
-    "updated_at": "2026-03-12",
+    "Date Posted": "March 15, 2026",
     },
     {
     "id": 2,
     "title": "Python    ",
     "content": "Python is a versatile programming language that is widely used for web development, data science, artificial intelligence, and more.",
-    "created_at": "2026-03-12",
-    "updated_at": "2026-03-12",
+    "Date Posted": "March 15, 2026",
     },
 ]
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-def read_root():
-    return """
-    <html>
-    <body>
-    <h1>Welcome to the FastAPI API</h1>
-    <p>This is a simple API built with FastAPI</p>
-    <a href="/api/posts">Get Posts</a>
-    </body>
-    </html>
-    """
+@app.get("/", include_in_schema=False)
+@app.get("/posts", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html",{"posts": posts, "title": "Home"})
+    
 
 
 @app.get("/api/posts")
